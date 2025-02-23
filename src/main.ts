c5676c2d1a1c640b7d3cfb6b3a892302f140e117
Leonardo-Rozza <leonardo155964@gmail.com>
2025-02-23 11:39:02 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// import "./clean-code/05-dry";
+// import "./clean-code/06-clases-b";
+// import "./solid/01-srp";
+import "./solid/02-open-close-a";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+
+  <h1>Hello World</h1>
+
+  </div>
+`;
---
+++ src/solid/02-open-close-a.ts
@@ -0,0 +1,17 @@
+import { PhotosService, PostService, TodoService } from "./02-open-close-b";
+import { HttpClient } from "./02-open-close-c";
+
+(async () => {
+  //Creamos una instancia de nuestra clase, que contiene la
+  const http: HttpClient = new HttpClient();
+
+  const todoService = new TodoService(http);
+  const postService = new PostService(http);
+  const photosService = new PhotosService(http);
+
+  const todos = await todoService.getTodoItems();
+  const posts = await postService.getPosts();
+  const photos = await photosService.getPhotos();
+
+  console.log({ todos, posts, photos });
+})();
